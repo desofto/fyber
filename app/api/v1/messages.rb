7d76a176978923517396fe03ecb06da132bf0fe3
Dmitry Silchenko <dmitry@desofto.com>
2021-02-21 17:15:26 +0100

    Allow pagination for messages

--- app/api/v1/messages.rb
+++ app/api/v1/messages.rb
@@ -3,12 +3,17 @@ module API
     class Messages < Grape::API
       resources :messages do
         desc 'Get all messages'
+        params do
+          optional :offset, type: Integer, default: 0
+          optional :limit, type: Integer, default: 100
+          optional :sort, type: String, default: 'created_at desc'
+        end
         get do
           authorize! :index, ::Message
 
           messages = ::Message.where('messages.user_id = :user_id OR group_id = :group_id', user_id: current_user.id, group_id: current_user.group_id)
 
-          messages = messages.reorder(created_at: :desc).limit(100)
+          messages = messages.reorder(params[:sort]).offset(params[:offset]).limit(params[:limit])
 
           (present messages).to_json
         end
